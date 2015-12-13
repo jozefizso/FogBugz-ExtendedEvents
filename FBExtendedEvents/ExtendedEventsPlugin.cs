61efad2dcc90bee816da1b2d13581d12195f19cb
Jozef Izso <jozef.izso@gmail.com>
2015-12-13 22:36:26 +0100

    Added ability to display commit info in a bug detail view.

--- FBExtendedEvents/ExtendedEventsPlugin.cs
+++ FBExtendedEvents/ExtendedEventsPlugin.cs
@@ -1,16 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.Design;
 using System.Data;
-using System.Text;
 using FogCreek.FogBugz;
 using FogCreek.FogBugz.Plugins;
 using FogCreek.FogBugz.Plugins.Api;
+using FogCreek.FogBugz.Plugins.Entity;
 using FogCreek.FogBugz.Plugins.Interfaces;
 
 namespace FBExtendedEvents
 {
-    public class ExtendedEventsPlugin : Plugin, IPluginDatabase
+    public class ExtendedEventsPlugin : Plugin, IPluginDatabase, IPluginPseudoBugEvent
     {
         private const int DATABASE_SCHEMA_VERSION = 1;
 
@@ -36,6 +35,44 @@ namespace FBExtendedEvents
 
         public void DatabaseUpgradeAfter(int ixVersionFrom, int ixVersionTo, CDatabaseUpgradeApi apiUpgrade)
         {
+        }
+
+        public CPseudoBugEvent[] PseudoBugEvents(CBug bug, CBugEvent[] rgBugEvent)
+        {
+            int ixBug = bug.ixBug;
+
+            var selectQuery = this.api.Database.NewSelectQuery(CommitEventEntity.GetPluginTableName(this.api.Database));
+            selectQuery.AddSelect("*");
+            selectQuery.AddWhere("ixBug = @ixBug");
+            selectQuery.SetParamInt("@ixBug", ixBug);
+
+            var commitEvents = new List<CPseudoBugEvent>();
+
+            var ds = selectQuery.GetDataSet();
+            if (ds.Tables[0] != null && ds.Tables[0].Rows.Count > 0)
+            {
+                foreach (DataRow row in ds.Tables[0].Rows)
+                {
+                    int ixCommitEvent = Convert.ToInt32(row["ixCommitEvent"]);
+                    string sRevision = Convert.ToString(row["sRevision"]);
+                    string sAuthor = Convert.ToString(row["sAuthor"]);
+                    DateTime dtCommit = Convert.ToDateTime(row["dtCommit"]);
+                    string sDtCommit = this.api.TimeZone.DateTimeString(dtCommit);
+
+                    var sHtml = $@"<div id=""bugevent_{ixCommitEvent}_commit"" class=""bugevent detailed"" style=""background-color: #dff0f7;"">
+                                     <div id=""bugeventSummary_{ixCommitEvent}_pe"" class=""summary"">
+                                       <span class=""action"">Commit r{sRevision} by {sAuthor}</span>&nbsp;" +
+                                    $@"<span class=""date"" dir=""ltr"">{sDtCommit}</a></span>
+                                     </div>
+                                     <div id=""bugeventChanges_{ixCommitEvent}_pe"" class=""changes"" dir=""ltr""></div>
+                                   </div>";
+
+                    var evt = new CPseudoBugEvent(dtCommit, sHtml);
+                    commitEvents.Add(evt);
+                }
+            }
+
+            return commitEvents.ToArray();
         }
     }
 }
