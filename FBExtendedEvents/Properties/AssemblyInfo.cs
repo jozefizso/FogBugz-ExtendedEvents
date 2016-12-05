9caac71450b8f4befd72ffba042b668f4582095d
Jozef Izso <jozef.izso@gmail.com>
2016-12-06 00:02:52 +0100

    Update assembly information

--- FBExtendedEvents/Properties/AssemblyInfo.cs
+++ FBExtendedEvents/Properties/AssemblyInfo.cs
@@ -4,15 +4,20 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Extended Events")]
 [assembly: AssemblyDescription("Show events from external sources like Subversion, TeamCity and Jenkins.")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jozef Izso")]
 [assembly: AssemblyProduct("FBExtendedEvents")]
-[assembly: AssemblyCopyright("Copyright © 2015 Jozef Izso")]
+[assembly: AssemblyCopyright("Copyright © 2015-2016 Jozef Izso")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("dc011599-24b0-4a15-acc5-bc3fa9a446d8")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
 
 [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
