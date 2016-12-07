58ff0fe1f69aa2bf933b6b0219a382ac026a613a
Jozef Izso <jozef.izso@gmail.com>
2016-12-07 17:39:24 +0100

    Update post-commit hook script

--- scripts/svn-hooks/post-commit-fogbugz-ee.ps1
+++ scripts/svn-hooks/post-commit-fogbugz-ee.ps1
@@ -6,17 +6,16 @@ param(
 )
 
 $cFbUrl="http://localhost/fb"
-$cIxRepo="1"
 $sPluginId="FBExtendedEvents@goit.io"
 $sToken=""
 
-$sRevision=$rev
+$sCommitRevision=$rev
 
 $info = (& svnlook.exe info -r $rev $repo) -split "`n"
 
-$sAuthor=$info[0]
+$sPersonName=$info[0]
 $dtCommit = [datetime]::Parse($info[1].Substring(0, 25)).ToUniversalTime()
-$sDateCommit =$dtCommit.ToString('o')
+$dtEventUtc =$dtCommit.ToString('o')
 
 $message = (& svnlook.exe log -r $rev $repo)
 $bugzIds = [regex]::matches($message, '\s*BugzID\s*[: ]+(\d+)') | % { $_.Groups[1].Value }
@@ -24,18 +23,18 @@ $sMessage = $message -join "`n"
 
 $bugzIds | foreach {
     $ixBug = $_
-    
+
     $params = @{
-      "sAction" = "commit"
-      "ixBug" = $ixBug
-      "ixRepository" = $cIxRepo
-      "sRevision" = $sRevision
-      "dtCommit" = $sDateCommit
-      "sAuthor" = $sAuthor
-      "sMessage" = $sMessage
+      "sAction" = "event"
+      "ixBug"= $ixBug
+      "sEventType" = "commit"
+      "dtEventUtc"= $dtEventUtc
+      "sPersonName" = $sPersonName
+      "sMessage"= $sMessage
+      "sCommitRevision"= $sCommitRevision
       "token" = $sToken
     }
-    
+
     $url = "$cFbUrl/default.asp?pg=pgPluginRaw&sPluginId=$sPluginId"
     try {
         $tmp = Invoke-WebRequest -Uri $url -Method GET -Body $params -TimeoutSec 30 -ErrorAction SilentlyContinue
