5d23fd2091af41272f615d47a459299a475d9555
Jozef Izso <jozef.izso@gmail.com>
2016-12-08 13:32:40 +0100

    Bug fix for parsing dates on .NET 2.0

--- FBExtendedEvents/DatabaseHelpers.cs
+++ FBExtendedEvents/DatabaseHelpers.cs
@@ -28,12 +28,29 @@ namespace FBExtendedEvents
             return defaultValue;
         }
 
+        // bugfix for .NET 2.0
+        // .NET 2.0 cannot parse ISO format from SVN log by default because it expects exactly 7 digits at microseconds
+        // 2015-01-01T17:47:27.064563Z => will fail on .NET 2.0, works on newer .NET runtimes
+        // 2015-01-01T17:47:27.0645630Z => will work on .NET 2.0
+        // 
+        // Supported formats:
+        // 2015-01-01T17:47:27.0645630Z
+        // 2015-01-01T17:47:27.064563Z
+        // 2015-01-01T17:47:27Z
+        // 2015-01-01T17:47:27.0000000+01:00
+        private static string[] ROUNDTRIP_DATE_FORMATS =
+        {
+            "yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fffffffK",
+            "yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'ffffffK",
+            "yyyy'-'MM'-'dd'T'HH':'mm':'ssK"
+        };
+
         public static DateTime GetDateTime(this CPluginRequest request, string name, DateTime defaultValue)
         {
             var val = request[name];
 
             DateTime value;
-            if (DateTime.TryParse(val, CultureInfo.InvariantCulture, DateTimeStyles.RoundtripKind, out value))
+            if (DateTime.TryParseExact(val, ROUNDTRIP_DATE_FORMATS, CultureInfo.InvariantCulture, DateTimeStyles.RoundtripKind, out value))
             {
                 return value;
             }
