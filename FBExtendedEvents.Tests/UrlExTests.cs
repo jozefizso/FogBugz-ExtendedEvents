f5274d3d116f142c90fbf95a7283f330b37b3a55
Jozef Izso <jozef.izso@gmail.com>
2016-12-10 14:10:22 +0100

    Add tests for UrlEx class

---
+++ FBExtendedEvents.Tests/UrlExTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace FBExtendedEvents.Tests
+{
+    [TestFixture]
+    public class UrlExTests
+    {
+        public static IEnumerable<object[]> PluginIds
+        {
+            get
+            {
+                yield return new object[] { "plugin1", "default.asp?pg=pgPluginRaw&sPlugin=plugin1" };
+                yield return new object[] { "FBExtendedEvents@goit.io", "default.asp?pg=pgPluginRaw&sPlugin=FBExtendedEvents%40goit.io" };
+            }
+        }
+
+        [Test]
+        [TestCaseSource(nameof(PluginIds))]
+        public void PluginRawPageUrl_PluginId_ReturnsUrlEncodedPathToPlugin(string pluginId, string expectedPluginUrl)
+        {
+            // Arrange & Act
+            var actualPluginUrl = UrlEx.PluginRawPageUrl(pluginId);
+
+            // Assert
+            Assert.AreEqual(expectedPluginUrl, actualPluginUrl);
+        }
+    }
+}
